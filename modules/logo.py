7cee17ef268c8b9ae11dd5dce3032ec505935308
JRIC2002 <jric2002kalilinux@gmail.com>
2020-04-30 22:19:13 -0500

    Updated modules

--- modules/logo.py
+++ modules/logo.py
@@ -16,7 +16,8 @@ def logo():
     """ Imprime el logo de la herramienta GeoTrackerIP. """
 
     #Foreground
-    black = "\033[1;30m"
+    black = "\033[0;30m"
+    gray = "\033[1;30m"
     red = "\033[1;31m"
     green = "\033[1;32m"
     yellow = "\033[1;33m"
@@ -26,7 +27,8 @@ def logo():
     white = "\033[1;37m"
     
     #Background
-    b_black = "\033[1;40m"
+    b_black = "\033[0;40m"
+    b_gray = "\033[1;40m"
     b_red = "\033[1;41m"
     b_green = "\033[1;42m"
     b_yellow = "\033[1;43m"
