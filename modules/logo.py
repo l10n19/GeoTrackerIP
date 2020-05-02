6af9e3ba39546ecdc7d327b4f3cf3400a0efd2b2
JRIC2002 <jric2002kalilinux@gmail.com>
2020-05-02 06:53:06 -0500

    Updated modules

--- modules/logo.py
+++ modules/logo.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" Logo de la herramienta GeoTrackerIP. """
+""" Inicio de la herramienta GeoTrackerIP. """
 
 __author__ = "JRIC2002"
 __copyright__ = "Copyright 2019, JRIC2002"
@@ -12,41 +12,75 @@ __maintainer__ = "JRIC2002"
 __email__ = "joselito18032002@gmail.com"
 __status__ = "Production"
 
-def logo():
-    """ Imprime el logo de la herramienta GeoTrackerIP. """
+class MyClass:
+    def __init__(self):
+        """ Variables de instancia. """
 
-    #Foreground
-    black = "\033[0;30m"
-    gray = "\033[1;30m"
-    red = "\033[1;31m"
-    green = "\033[1;32m"
-    yellow = "\033[1;33m"
-    blue = "\033[1;34m"
-    magenta = "\033[1;35m"
-    cyan = "\033[1;36m"
-    white = "\033[1;37m"
+        #COLORS
+
+        #Foreground
+        self.black = "\033[0;30m"
+        self.gray = "\033[1;30m"
+        self.red = "\033[1;31m"
+        self.green = "\033[1;32m"
+        self.yellow = "\033[1;33m"
+        self.blue = "\033[1;34m"
+        self.magenta = "\033[1;35m"
+        self.cyan = "\033[1;36m"
+        self.white = "\033[1;37m"
+
+        #Background
+        self.b_black = "\033[0;40m"
+        self.b_gray = "\033[1;40m"
+        self.b_red = "\033[1;41m"
+        self.b_green = "\033[1;42m"
+        self.b_yellow = "\033[1;43m"
+        self.b_blue = "\033[1;44m"
+        self.b_magenta = "\033[1;45m"
+        self.b_cyan = "\033[1;46m"
+        self.b_white = "\033[1;47m"
     
-    #Background
-    b_black = "\033[0;40m"
-    b_gray = "\033[1;40m"
-    b_red = "\033[1;41m"
-    b_green = "\033[1;42m"
-    b_yellow = "\033[1;43m"
-    b_blue = "\033[1;44m"
-    b_magenta = "\033[1;45m"
-    b_cyan = "\033[1;46m"
-    b_white = "\033[1;47m"
+    def logo(self):
+        """ Imprime logo de la herramienta GeoTrackerIP. """
 
-    print("")
-    print("     {} __ ___ __ {}_____ ___  __   ____  _____ ___   {} _ ___ ".format(blue, green, cyan))
-    print("     {}/ _] __/__\{}_   _| _ \/  \ / _/ |/ / __| _ \{}__{}| | _,\ ".format(blue, green, white, cyan))
-    print("    {}| [/\ _| \/ |{}| | | v / /\ | \_|   <| _|| v /{}__{}| | v_/ ".format(blue, green, white, cyan))
-    print("     {}\__/___\__/ {}|_| |_|_\_||_|\__/_|\_\___|_|_\  {}|_|_|   {}v2.0 ".format(blue, green, cyan, white))
-    print("")
-    print("               {}<<< {}Tool coded by:{} @JRIC2002 {}>>>{}".format(red, yellow, white, red, white))
-    print("    {}<<< {}Description:{} Geolocate an IP address or Domain {}>>>{}".format(red, yellow, white, red, white))
-    print("")
+        print("")
+        print("     {} __ ___ __ {}_____ ___  __   ____  _____ ___   {} _ ___ ".format(self.blue, self.green, self.cyan))
+        print("     {}/ _] __/__\{}_   _| _ \/  \ / _/ |/ / __| _ \{}__{}| | _,\ ".format(self.blue, self.green, self.white, self.cyan))
+        print("    {}| [/\ _| \/ |{}| | | v / /\ | \_|   <| _|| v /{}__{}| | v_/ ".format(self.blue, self.green, self.white, self.cyan))
+        print("     {}\__/___\__/ {}|_| |_|_\_||_|\__/_|\_\___|_|_\  {}|_|_|   {}v2.0 ".format(self.blue, self.green, self.cyan, self.white))
+        print("")
+        print("               {}<<< {}Tool coded by:{} @JRIC2002 {}>>>{}".format(self.red, self.yellow, self.white, self.red, self.white))
+        print("    {}<<< {}Description:{} Geolocate an IP address or Domain {}>>>{}".format(self.red, self.yellow, self.white, self.red, self.white))
+        print("")
 
+    def help_menu(self):
+        """ Imprime el menú de ayuda de la herramienta GeoTrackerIP. """
+
+        print("{}Usage: python3 GeoTrackerIP.py [options]".format(self.white))
+        print("")
+        print("Options:")
+        print("   -h, --help              Show this help message and exit.")
+        print("   -v, --version           Show program's version number and exit.")
+        print("")
+        print("   Target:")
+        print("      At least one of these options has to be provided to define the target(s).")
+        print("")
+        print("      -t, --target            IP Address or Domain to be analyzed.")
+
+    def version(self):
+        """ Imprime la version de la herramienta GeoTrackerIP. """
+
+        print("{}#GeoTrackerIP version 2.0".format(self.white))
+    
+    def error_args(self):
+        """ Imprime un mensaje de error de argumentos. """
+
+        print("{}Usage: python3 GeoTrackerIP.py [options]".format(self.white))
+        print("")
+        print("GeoTrackerIP.py: Error: Invalid option.")
+        print("Use -h or --help to see the options.")
+        
 #START
+function = MyClass()
 if __name__ == "__main__":
-    logo()
+    function.logo()
